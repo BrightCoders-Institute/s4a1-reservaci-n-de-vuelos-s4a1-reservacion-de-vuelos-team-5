d311007d49a176bb93172535bb9ae88f1ee51d91
Carlos S. <carlossalmeroncabrera@gmail.com>
2024-01-26 16:03:02 -0600

    Modified all screen styles

--- src/components/BtnNext.tsx
+++ src/components/BtnNext.tsx
@@ -1,33 +1,30 @@
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
-import React from 'react'
+import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import React from 'react';
 import {useNavigation} from '@react-navigation/native';
 
-
-export default function BtnNext({next}:any) {
-  const route = next.route
-  const params = next.params
+export default function BtnNext({next}: any) {
+  const route = next.route;
+  const params = next.params;
   const navigation = useNavigation();
   return (
-     <TouchableOpacity
-    onPress={()=>navigation.navigate(route,params)}
-    >
-        <View style={styles.container}>
-          <Text style={styles.txt}>Next</Text>
-        </View> 
+    <TouchableOpacity onPress={() => navigation.navigate(route, params)}>
+      <View style={styles.container}>
+        <Text style={styles.txt}>Next</Text>
+      </View>
     </TouchableOpacity>
-    )
+  );
 }
 
 const styles = StyleSheet.create({
-  container:{
+  container: {
     backgroundColor: '#5C6EF8',
     alignItems: 'center',
     width: 280,
-    borderRadius:7,
+    borderRadius: 7,
     paddingVertical: 8,
     paddingHorizontal: 15,
-  }, 
-  txt:{
-    color:'white'
-  }
-})
+  },
+  txt: {
+    color: 'white',
+  },
+});
--- src/screens/flight_log_screens/5_FinalRegister.tsx
+++ src/screens/flight_log_screens/5_FinalRegister.tsx
@@ -1,41 +1,54 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import BtnNext from '../../components/BtnNext'
-import CardProcess from '../../components/CardProcess'; 
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import BtnNext from '../../components/BtnNext';
+import CardProcess from '../../components/CardProcess';
 
 export default function FinalRegister({route}) {
-  const {origin, destiny,date,passengers} = route.params
+  const {origin, destiny, date, passengers} = route.params;
   return (
-    <View style={{position:"relative", flex: 1,padding: 20}}>
-      <CardProcess nameO = {origin} countryO='' nameD={destiny} countryD='' date={date} passengers={`${passengers+1} passengers`}/>
+    <View style={styles.containerView}>
+      <View style={{marginTop: 130}} />
+      <CardProcess
+        nameO={origin}
+        countryO=""
+        nameD={destiny}
+        countryD=""
+        date={date}
+        passengers={`${passengers + 1} passengers`}
+      />
       <View style={styles.containerInput}>
         <Text style={styles.title}>Your request {'\n'}was received.</Text>
       </View>
       <View style={styles.btn}>
-        <BtnNext next={'FinalRegister'}></BtnNext>
+        <BtnNext next={'FinalRegister'} />
       </View>
     </View>
-      )
-    }
-    
-    const styles = StyleSheet.create({
-      containerInput: {
-        marginTop: 100,
-      },
-      title: {
-        fontSize: 35,
-        fontWeight: 'bold',
-        margin:14,
-        marginBottom: 15,
-        color: 'black',
-      },
-      btn:{
-        display: "flex",
-        justifyContent: "center",
-        alignItems:'center',
-        position: 'absolute',
-        left: "45%",
-        top: '90%',
-        transform: [{ translateX: -100 }] //mitad del ancho del boton
-      }
-    })
+  );
+}
+
+const styles = StyleSheet.create({
+  containerView: {
+    position: 'relative',
+    flex: 1,
+    padding: 20,
+  },
+  containerInput: {
+    marginTop: 20,
+  },
+  title: {
+    fontSize: 35,
+    fontWeight: 'bold',
+    margin: 14,
+    marginBottom: 15,
+    color: 'black',
+  },
+  btn: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    position: 'absolute',
+    left: '45%',
+    top: '90%',
+    transform: [{translateX: -100}], //mitad del ancho del boton
+  },
+});
