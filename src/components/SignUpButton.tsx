c3fa4aacad246af3642564ba952bbb0683471a34
yahir00s <yairjesus49@hotmail.com>
2024-01-30 20:39:27 -0600

    auth firebase fix

--- src/components/SignUpButton.tsx
+++ src/components/SignUpButton.tsx
@@ -13,7 +13,6 @@ interface SignUpProps {
 
 const SignUpButton = ({title,email,password}:SignUpProps) => {
   const navigation = useNavigation();
-  const [isValid, setIsValid] = useState(false);
 
   function validate(){
     const emailPattern =  /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
@@ -22,19 +21,19 @@ const SignUpButton = ({title,email,password}:SignUpProps) => {
       auth().createUserWithEmailAndPassword(`${email}`, `${password}`)
       Alert.alert('Éxito');
       navigation.navigate('Home')
-      setIsValid(true)
     } else {
       Alert.alert('Error', '¡El email o contraseña no validos!');
-      setIsValid(false)
+
     }
   }
-
+  const len = () => (password.length < 1 || email.length < 1 ? false : true)
+  
   return (
     <TouchableOpacity
       style={styles.Button}
-      disabled= {!isValid}
+      disabled={!len()}
       onPress={validate}>
-      <View style={isValid ? styles.containerButton : styles.containerButtonFalse}>
+      <View style={[styles.containerButton, !len() && styles.containerButtonFalse]}>
         <Text style={styles.Btn_txt}>{title}</Text>
       </View>
     </TouchableOpacity>
